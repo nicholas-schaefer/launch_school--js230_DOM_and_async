d9e9614c4e54dbaea7eb5dfcbacb8a98640f1598
Nicholas Schaefer <nick@nicholasschaefer.com>
2025-03-22 16:34:28 -0400

    playing handlebars

--- lessons/4_libraries/11_handlebars/javascripts/app.js
+++ lessons/4_libraries/11_handlebars/javascripts/app.js
@@ -1,26 +1,30 @@
 (function notes(){
   `
-  Locate the template by ID and compile it to a template function.
-  Render the post to the body element using the function.
+Create a posts array, adding the existing post to it.
+Add a second post with no tags property.
+Modify your template to check for the existence of tags, and if none exist, output a "Not tagged" message.
+Wrap the template in an each loop to output each post.
   `
 });
 
 let post = {
   title: 'Lorem ipsum dolor sit amet',
   published: 'April 1, 2015',
-  body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.'
+  body: 'Sed ut <strong>perspiciatis</strong> unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.',
+  tags: ['catfluencer', 'muffin', 'warrior'],
 };
+
 
 document.addEventListener('DOMContentLoaded', (event) =>{
   let postEl = document.querySelector('#post');
   let postTemplate = Handlebars.compile(postEl.innerHTML);
 
   let html = postTemplate(post);
-  console.log(html)
-
   let articles = document.getElementById('articles');
 
   articles.innerHTML = html
+
+  Handlebars.registerPartial('myPartial', '{{prefix}}');
 
   // const parser = new DOMParser();
 
