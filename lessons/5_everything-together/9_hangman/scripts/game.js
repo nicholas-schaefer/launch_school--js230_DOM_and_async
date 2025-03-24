c9d6eafa777268fcab156fd3e4986c8f9ddc5d0d
Nicholas Schaefer <nick@nicholasschaefer.com>
2025-03-24 13:10:50 -0400

    hangman completed

--- lessons/5_everything-together/9_hangman/scripts/game.js
+++ lessons/5_everything-together/9_hangman/scripts/game.js
@@ -23,42 +23,77 @@ class Game{
 
   constructor(documentObject){
     this.document = documentObject;
-    this.wordPool = ['apple', 'bat', 'cat'];
+    this.bindEvents();
+    this.wordPool = ['APPLE', 'BAT', 'CAT'];
     this.newGame();
   }
 
   newGame(){
     const selectedWord = this.randomWord();
-    if (!selectedWord) {
-      gameOverNoMoreWords();
-      return;
+    const outOfWords = !selectedWord;
+    console.log(selectedWord, outOfWords, this.wordPool.length)
+
+    if (outOfWords) {
+      this.handleNoMoreWords()
+      return
     }
+
+    this.clearGameState();
+
+    this.clearMessage();
+    this.clearSpaces();
+    this.clearGuesses();
+    this.clearCSS();
+
     this.secretWord = selectedWord;
+    this.secretWordUniqueCharsCount = new Set(selectedWord.split('')).size;
+    this.correctGuesses = new Set();
     this.incorrectGuesses = new Set();
     this.lettersGuessed = new Set();
+    this.currentlyAcceptingGuesses = true;
     this.draw();
-    this.bindEvents();
+    // this.bindEvents();
   };
 
   get incorrectGuessesCount(){
     return this.incorrectGuesses.size;
   }
 
-  hasGuessLimitBeenReached(){
-    const guessLimit = 6;
+  get correctGuessesCount(){
+    return this.correctGuesses.size;
+  }
 
+  isGameWon(){
+    return this.secretWordUniqueCharsCount === this.correctGuessesCount;
+  }
+
+  isGameLost(){
+    const guessLimit = 6;
     return this.incorrectGuessesCount === guessLimit;
   }
 
-  //TODO: write this logic
-  gameOverAllWordsUsed(){
-    console.log("Sorry, I've run out of words!")
+  handleWin(){
+    let message = this.document.querySelector('#message');
+    message.textContent = "You Win!";
+    this.document.body.classList.add('win');
   }
+
+  handleLoss(){
+    let message = this.document.querySelector('#message');
+    message.textContent = "You lose"
+    this.document.body.classList.add('lose');
+  }
+
+  handleNoMoreWords(){
+    let message = this.document.querySelector('#message');
+    message.textContent = "Sorry! You're out of words"
+  }
+
 
   randomWord(){
     if (this.wordPool.length === 0) return;
 
-    const randomIdx = Math.floor(Math.random() * 3);
+    const randomIdx = Math.floor(Math.random() * this.wordPool.length);
     const word = this.wordPool[randomIdx];
     this.wordPool.splice(randomIdx, 1);
 
@@ -70,21 +105,54 @@ class Game{
     this.drawGuesses();
   }
 
-  //TODO: change hardcoded guess
   drawSpaces(){
     let spaces = document.querySelector('#spaces');
 
     let table = this.document.createElement('table');
     let tr = this.document.createElement('tr');
-    table.append(tr)
 
-    let guess = "a";
+    this.clearSpaces();
     this.secretWord.split('').forEach((char) =>{
       const td = this.document.createElement('td');
-      td.textContent = guess === char ? char : '_';
+      td.textContent = this.correctGuesses.has(char) ? char : '_';
       tr.append(td);
     });
+    table.append(tr)
     spaces.append(table);
+  }
+
+  clearGameState(){
+    this.clearMessage();
+    this.clearSpaces();
+    this.clearGuesses();
+    this.clearCSS();
+  }
+
+  clearMessage(){
+    let message = this.document.querySelector('#message');
+    message.innerHTML = "";
+  }
+
+  clearSpaces(){
+    let spacesTable = this.document.querySelector('#spaces > table');
+    spacesTable?.remove();
+  }
+
+  clearGuesses(){
+    let guessesTable = this.document.querySelector('#guesses > table');
+    guessesTable?.remove();
+  }
+
+  clearCSS(){
+    this.document.body.classList.remove('win', 'lose');
+
+    let apples = this.document.getElementById('apples');
+    [...apples.classList].forEach((className) => {
+      if (className.startsWith('guess_')) {
+        apples.classList.remove(className);
+      }
+    })
+
   }
 
   drawGuesses(){
@@ -92,29 +160,73 @@ class Game{
 
     let table = this.document.createElement('table');
     let tr = this.document.createElement('tr');
-    table.append(tr)
 
-    this.secretWord.split('').forEach((char) =>{
+    this.clearGuesses();
+    this.lettersGuessed.forEach((incorrectGuess) =>{
       const td = this.document.createElement('td');
-      td.textContent = this.lettersGuessed.has(char) === char ? char : '_';
+      td.textContent = incorrectGuess;
       tr.append(td);
-    });
+    })
+
+    table.append(tr)
     guesses.append(table);
   }
 
   bindEvents(){
-    this.document.addEventListener("keydown", (event) =>{
+    this.document.addEventListener("keydown", (event) => this.handleKeydown(event));
+    this.document.addEventListener("click", (event) => this.handleClick(event));
+  }
 
-      function isKeyALetter(key){
-        return key.length === 1 && /[a-z]/i.test(key)
-      }
+  handleClick(event){
+    const target = event.target;
+    if (target.id === "replay"){
+      event.preventDefault();
+      this.newGame()
+    }
+  }
 
-      if (!isKeyALetter(event.key)) return;
-      const keyUpcased = event.key.toUpperCase();
+  handleKeydown(event){
+    if (!this.currentlyAcceptingGuesses) return;
 
-      console.log(keyUpcased)
+    function isKeyALetter(key){
+      return key.length === 1 && /[a-z]/i.test(key);
+    }
 
-    })
+    if (!isKeyALetter(event.key)) return;
+    this.makeGuess(event.key);
+    this.draw();
+    if (this.isGameWon()) return this.handleWin();
+    if (this.isGameLost()) return this.handleLoss();
+
+  }
+
+  //TODO: this should probably be made private
+  makeGuess(letter){
+    const letterUpcased = letter.toUpperCase();
+    const letterWasAlreadyGuessed = this.lettersGuessed.has(letterUpcased);
+    if (letterWasAlreadyGuessed) return;
+
+    this.lettersGuessed.add(letterUpcased);
+
+    const isGuessCorrect = new RegExp(letterUpcased).test(this.secretWord);
+
+    isGuessCorrect
+    ? this.handleCorrectGuess(letterUpcased)
+    : this.handleIncorrectGuess(letterUpcased)
+  }
+
+  //TODO: this should probably be made private
+  handleCorrectGuess(correctLetter){
+    this.correctGuesses.add(correctLetter)
+  }
+
+  //TODO: this should probably be made private
+  handleIncorrectGuess(incorrectLetter){
+    this.incorrectGuesses.add(incorrectLetter)
+
+    let apples = this.document.getElementById('apples');
+    const newClass = "guess" + "_" + this.incorrectGuessesCount;
+    apples.classList.add(newClass);
   }
 
 
@@ -123,18 +235,5 @@ class Game{
 
 document.addEventListener("DOMContentLoaded", (event) =>{
   const game = new Game(document);
-
-  // document.addEventListener("keydown", (event) =>{
-
-  //   function isKeyALetter(key){
-  //     return key.length === 1 && /[a-z]/i.test(key)
-  //   }
-
-  //   if (!isKeyALetter(event.key)) return;
-  //   const keyUpcased = event.key.toUpperCase();
-
-  //   console.log(keyUpcased)
-
-  // })
 
 })
